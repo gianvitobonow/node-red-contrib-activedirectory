9120e157dff88cec369526aabbadebb7eccfe11c
Gianvito Bono <gianvito.bono@h-farm.com>
2020-10-30 09:25:30 +0100

    Change MaxPageSize

--- nodes/query.js
+++ nodes/query.js
@@ -1,65 +1,67 @@
 module.exports = function(RED) {
 
-  function queryNode(config) {
-    RED.nodes.createNode(this,config);
-    var node = this;
-    // we get the properties
-    node.url = config.url;
-    node.baseDN = config.baseDN;
-    // we get the credentials
-    var cUsername = this.credentials.username;
-    var cPassword = this.credentials.password;
-    node.on('input', function(msg) {
-      node.status({fill:"blue", shape:"ring", text:"connecting"});
-      // import activedirectory2
-      var ActiveDirectory = require('activedirectory2');
-      var adConfig = {
-        url: node.url,
-        baseDN: node.baseDN,
-        tlsOptions: node.tlsOptions,
-        username: cUsername,
-        password: cPassword
-      };
-      // set attributes if defined
-      if (msg.ad_attributes) {
-        // Validates the Object format (required for IBMi platform)
-        adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes));
-      }
-      if (msg.tlsOptions) {
-        // Validates the Object format (required for IBMi platform)
-        adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions));
-      }
-      try {
-        var ad = new ActiveDirectory(adConfig);
-        node.status({fill:"green", shape:"dot", text:"connected"});
-        var query = msg.payload;
-        var opts = {
-          includeMembership : [ 'group', 'user' ], // Optionally can use 'all'
-          includeDeleted : false
-        };
-        node.status({fill:"blue",shape:"ring",text:"querying"});
-        ad.find(query, function(err, results) {
-          if (err) {
-            node.status({fill:"red", shape:"dot", text:"error querying"});
-            node.error('ERROR querying: ' + JSON.stringify(err));
-            return;
-          }
-          msg.payload = results;
-          node.status({});
-          node.send(msg);
-        });
-      } catch(e) {
-        node.status({fill:"red", shape:"dot", text:"connexion error"});
-        node.error('ERROR connecting: ' + e.message);
-      }
-    });
-  }
+    function queryNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        // we get the properties
+        node.url = config.url;
+        node.baseDN = config.baseDN;
+        // we get the credentials
+        var cUsername = this.credentials.username;
+        var cPassword = this.credentials.password;
+        node.on('input', function(msg) {
+            node.status({ fill: "blue", shape: "ring", text: "connecting" });
+            // import activedirectory2
+            var ActiveDirectory = require('activedirectory2');
+            var adConfig = {
+                url: node.url,
+                baseDN: node.baseDN,
+                tlsOptions: node.tlsOptions,
+                username: cUsername,
+                password: cPassword,
+                pageSize: 50000
+            };
+            // set attributes if defined
+            if (msg.ad_attributes) {
+                // Validates the Object format (required for IBMi platform)
+                adConfig.attributes = JSON.parse(JSON.stringify(msg.ad_attributes));
+            }
+            if (msg.tlsOptions) {
+                // Validates the Object format (required for IBMi platform)
+                adConfig.tlsOptions = JSON.parse(JSON.stringify(msg.tlsOptions));
+            }
+            try {
+                var ad = new ActiveDirectory(adConfig);
+                node.status({ fill: "green", shape: "dot", text: "connected" });
+                var query = msg.payload;
+                var opts = {
+                    includeMembership: ['group', 'user'], // Optionally can use 'all'
+                    includeDeleted: false,
 
-  RED.nodes.registerType("query",queryNode,{
-    credentials: {
-      username: {type:"text"},
-      password: {type:"password"}
+                };
+                node.status({ fill: "blue", shape: "ring", text: "querying" });
+                ad.find(query, function(err, results) {
+                    if (err) {
+                        node.status({ fill: "red", shape: "dot", text: "error querying" });
+                        node.error('ERROR querying: ' + JSON.stringify(err));
+                        return;
+                    }
+                    msg.payload = results;
+                    node.status({});
+                    node.send(msg);
+                });
+            } catch (e) {
+                node.status({ fill: "red", shape: "dot", text: "connexion error" });
+                node.error('ERROR connecting: ' + e.message);
+            }
+        });
     }
-  });
+
+    RED.nodes.registerType("query", queryNode, {
+        credentials: {
+            username: { type: "text" },
+            password: { type: "password" }
+        }
+    });
 
 }
